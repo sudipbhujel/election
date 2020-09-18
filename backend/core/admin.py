1628fbee9060a0dfc2aa8920c9419f7a16b895df
Sudip Bhujel <devsudipbhujel@gmail.com>
2020-09-18 20:40:25 +0545

    adds Profile, Candidate and Party models and admin site ✌

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from .models import FaceImage, User
+from .models import Candidate, FaceImage, Party, Profile, User
 
 
 class InlineFaceImage(admin.StackedInline):
@@ -39,5 +39,57 @@ class FaceImageAdmin(admin.ModelAdmin):
     list_display_links = ('id',)
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    date_hierarchy = ('date_submitted')
+    readonly_fields = ('date_submitted', 'date_edited', )
+    list_display = ('id', 'first_name', 'last_name', 'is_voter', 'is_voted')
+    search_fields = ('first_name', 'last_name', 'municipality', 'tole')
+    list_filter = ('is_voter', 'is_voted', 'gender')
+    fieldsets = (
+        (_('Personal Info'), {'fields': ('user', 'first_name', 'last_name',
+                                         'dob', 'gender',
+                                         'citizenship_issued_district',
+                                         'citizenship')}),
+        (_('Address info'), {
+         'fields': (('province', 'district'), ('municipality', 'ward'),
+                    'tole')}),
+        (_('Roles'), {
+         'fields': ('is_voter', 'is_voted')}),
+        (_('Dates'), {'fields': ('date_submitted', 'date_edited')}),
+    )
+
+    radio_fields = {'gender': admin.HORIZONTAL}
+
+
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    fieldsets = (
+        (None, {'fields': ('name', 'description', 'slogan', 'logo')}),
+        (_('Election Info'), {
+         'fields': ('manifesto', 'plans')}),
+        (_('Vote'), {
+         'fields': ('vote_count',)}),
+    )
+
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name')
+    list_filter = ('is_candidate',)
+    fieldsets = (
+        (_('Personal Info'), {'fields': ('profile', 'party', 'bio', 'plans')}),
+        (_('Role'), {
+         'fields': ('is_candidate',)}),
+        (_('Vote'), {
+         'fields': ('vote_count',)}),
+    )
+
+    def full_name(self, obj):
+        return f'{obj.profile.get_full_name}'
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(FaceImage, FaceImageAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Party, PartyAdmin)
+admin.site.register(Candidate, CandidateAdmin)
--- backend/core/models.py
+++ backend/core/models.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+from django.template.defaultfilters import slugify
+from django.core.validators import MinValueValidator
+
+from .choices import DISTRICT_CHOICES, GENDER_CHOICES, PROVINCE_CHOICES
 
 
 def user_face_image_file_path(instance, filename):
@@ -16,6 +20,39 @@ def user_face_image_file_path(instance, filename):
     filename = f'{uuid.uuid4()}.{ext}'
 
     return os.path.join('uploads/user/', str(instance.user), 'face', filename)
+
+
+def user_citizenship_image_file_path(instance, filename):
+    """
+    Generate file path for a user citizenship image
+    """
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/user/', str(instance.user),
+                        'citizenship', filename)
+
+
+def user_profile_image_file_path(instance, filename):
+    """
+    Generate file path for a user citizenship image
+    """
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/user/', str(instance.user),
+                        'profile', filename)
+
+
+def party_logo_image_file_path(instance, filename):
+    """
+    Generate file path for a user citizenship image
+    """
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    slug = str(slugify(instance.name))
+
+    return os.path.join('uploads/party/', slug, 'logo', filename)
 
 
 class UserManager(BaseUserManager):
@@ -95,7 +132,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _('users')
 
     def __str__(self):
-        return str(self.id)
+        return str(self.citizenship_number)
 
 
 class FaceImage(models.Model):
@@ -111,3 +148,129 @@ class FaceImage(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class Profile(models.Model):
+    """
+    A Profile models stores full featured details of voters.
+    """
+    id = models.UUIDField(_('id'), default=uuid.uuid4,
+                          primary_key=True, editable=False)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+
+    first_name = models.CharField(_('first name'), max_length=30)
+    last_name = models.CharField(_('last name'), max_length=30)
+    image = models.ImageField(
+        _('profile image'), upload_to=user_profile_image_file_path)
+    gender = models.CharField(_('gender'),
+                              max_length=15, choices=GENDER_CHOICES)
+    dob = models.DateField(_('date of birth'))
+
+    father_name = models.CharField(
+        _('father\'s name'), max_length=30)
+    mother_name = models.CharField(
+        _('mother\'s name'), max_length=30)
+    citizenship_issued_district = models.CharField(
+        _('citizenship issued district'), max_length=15,
+        choices=DISTRICT_CHOICES)
+    citizenship = models.ImageField(
+        _('citizenship image'), upload_to=user_citizenship_image_file_path)
+
+    # Custom roles
+    is_voter = models.BooleanField(_('voter'), default=False,
+                                   help_text=_(
+                                       'Designates whether the \
+                                        user can vote.'))
+    is_voted = models.BooleanField(_('voted'), default=False,
+                                   help_text=_(
+                                       'Designates whether the user \
+                                        casted vote.'))
+
+    # Address Information
+    province = models.CharField(
+        _('province number'), max_length=15, choices=PROVINCE_CHOICES)
+    district = models.CharField(
+        _('district'), max_length=15, choices=DISTRICT_CHOICES)
+    municipality = models.CharField(
+        _('municipality'), max_length=30)
+    ward = models.IntegerField(_('ward number'))
+    tole = models.CharField(_('tole'), max_length=30)
+
+    # # Contact Information
+    # phone = models.IntegerField(_('contact number'))
+
+    # Dates
+    date_submitted = models.DateTimeField(
+        _('submission date'), default=timezone.now)
+    date_edited = models.DateTimeField(
+        _('edit date'), auto_now=True)
+
+    @property
+    def get_full_name(self):
+        """
+        Returns the first_name plus last_name, with a space in between.
+        """
+        full_name = f'{self.first_name} {self.last_name}'
+        return full_name.strip()
+
+    def __str__(self):
+        return str(self.get_full_name)
+
+
+class Party(models.Model):
+    """
+    A Party model stores political party informations.
+    """
+    id = models.UUIDField(_('id'), default=uuid.uuid4,
+                          primary_key=True, editable=False)
+    name = models.CharField(_('name'), max_length=25)
+    description = models.TextField(_('description'))
+    slogan = models.TextField(_('slogan'))
+    logo = models.ImageField(
+        _('logo'), upload_to=party_logo_image_file_path)
+    manifesto = models.TextField(_('manifesto'))
+    plans = models.TextField(_('plans'))
+
+    vote_count = models.IntegerField(_('vote count'), default=0,
+                                     validators=[MinValueValidator(0)])
+
+    class Meta:
+        verbose_name = _('party')
+        verbose_name_plural = _('parties')
+
+    @property
+    def get_vote_count(self):
+        """
+        Returns the total vote count of party.
+        """
+        return f'{self.vote_count}'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Candidate(models.Model):
+    """
+    A Candidate model stores candidate informations.
+    """
+    id = models.UUIDField(_('id'), default=uuid.uuid4,
+                          primary_key=True, editable=False)
+    profile = models.OneToOneField(
+        Profile, on_delete=models.CASCADE, related_name='candidate')
+    party = models.OneToOneField(
+        Party, on_delete=models.CASCADE, related_name='party')
+    bio = models.TextField(_('bio'))
+    plans = models.TextField(_('plans'))
+
+    # Role
+    is_candidate = models.BooleanField(_('candidate'),
+                                       default=False,
+                                       help_text=_(
+        'Designates whether the user is candidate'))
+
+    vote_count = models.IntegerField(_('vote count'), default=0,
+                                     validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return str(self.profile.get_full_name)
