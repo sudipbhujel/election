cc9b80355b7b6e5866a5a2a87354180fe4b17c08
Sudip Bhujel <devsudipbhujel@gmail.com>
2020-10-27 19:41:51 +0545

    refactors interceptor code 🤞

--- frontend-react/src/store/config.auth.js
+++ frontend-react/src/store/config.auth.js
@@ -8,55 +8,60 @@ export const api = (store) => {
       return response;
     },
     (error) => {
-      return new Promise((resolve) => {
-        const originalRequest = error.config;
-        const refreshToken = loadState().auth.data.refresh.token;
-        if (!refreshToken) {
-          throw error;
-        }
-        if (
-          error.response &&
-          error.response.status === 401 &&
-          error.config &&
-          !error.config.__isRetryRequest &&
-          refreshToken
-        ) {
-          originalRequest._retry = true;
+      if (error.config.headers["Authorization"]) {
+        return new Promise((resolve) => {
+          const originalRequest = error.config;
+          const refreshToken = loadState().auth.data.refresh.token;
+          if (!refreshToken) {
+            throw error;
+          }
+          if (
+            error.response &&
+            error.response.status === 401 &&
+            error.config &&
+            !error.config.__isRetryRequest &&
+            refreshToken
+          ) {
+            originalRequest._retry = true;
 
-          const response = fetch(baseURL + "/api/user/token/refresh/", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              refresh: refreshToken,
-            }),
-          })
-            .then((res) => {
-              if (res.ok) return res.json();
-              // history.push('/login')
-              // throw error;
+            const response = fetch(baseURL + "/api/user/token/refresh/", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                refresh: refreshToken,
+              }),
             })
-            .then((res) => {
-              store.dispatch({
-                type: "REFRESH_TOKEN_REQUEST",
-                payload: { access: res.access, refresh: refreshToken },
+              .then((res) => {
+                if (res.ok) return res.json();
+                // history.push('/login')
+                // throw error;
+              })
+              .then((res) => {
+                store.dispatch({
+                  type: "REFRESH_TOKEN_REQUEST",
+                  payload: { access: res.access, refresh: refreshToken },
+                });
+                originalRequest.headers[
+                  "Authorization"
+                ] = `Bearer ${res.access}`;
+                return axios(originalRequest);
+              })
+              .catch((error) => {
+                // Refresh failure
+                store.dispatch({
+                  type: "REFRESH_TOKEN_REQUEST",
+                  payload: {},
+                });
+                return Promise.reject(error);
               });
-              originalRequest.headers["Authorization"] = `Bearer ${res.access}`;
-              return axios(originalRequest);
-            })
-            .catch((error) => {
-              // Refresh failure
-              store.dispatch({
-                type: "REFRESH_TOKEN_REQUEST",
-                payload: {},
-              });
-              return Promise.reject(error);
-            });
-          resolve(response);
-        }
-        console.log("Before Promise.reject(error)");
-      });
+            resolve(response);
+          }
+        });
+      }
+      delete error.config.headers["Authorization"];
+      throw error;
     }
   );
 
